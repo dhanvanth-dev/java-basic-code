96483ad4aae7ef6ccbfaab33aa8be6dfaf029839
dhanvanth-dev <dhanvanthkrishnan@gmail.com>
2025-07-27 02:51:49 +0530

    print pairs of an array

---
+++ printPairs.java
@@ -0,0 +1,16 @@
+
+public class printPairs{
+    public static void Printpairs(int numbers[]){
+        for(int i=0;i<numbers.length;i++){
+            int curr = numbers[i];
+            for (int j=i+1 ;j<numbers.length ;j++){
+                System.out.println("(" + curr + "," + numbers[j] + ")");
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int numbers[] = {2,4,6,8,10};
+        Printpairs(numbers);
+    }
+}
